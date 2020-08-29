6518da2042039e7aa7bd46cc3371b9b90e31bace
kayain73 <kayain73@gmail.com>
2020-08-30 01:06:51 +0900

    final code

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "Freedom is the Freedom to say that two plus two make four. IF that is granted, all else follow"
+            </span>
+            <span> - Georage Orwll, 1984</span>
+        </div>
+    )
+}
+
+export default About;
+
